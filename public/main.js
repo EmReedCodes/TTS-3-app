17e44cb8e83143bfb8203b06b2a885c53548aa1f
Emily Reed <emilyreed89@gmail.com>
2022-07-01 14:53:56 -0400

    changed fetch async

--- public/main.js
+++ public/main.js
@@ -1,27 +1,56 @@
 // TODO; look into browserify so I can import npm modules to client side
+// let wordData = []
+// let randomWord
+// let getContent
 
+// fetch('http://localhost:8010/api/foo')
+// .then((res) => res.json())
+// .then(data => {
+//     data.forEach(item => {
+//          //console.log(item.content)
+//         wordData.push(item.content)
+//         //item = item.content
+//     })
+//     randomWord = getContent[Math.floor(Math.random() * getContent.length)]
+//     //getContent = data.map(item => item.content)
+//   //console.log(getContent)
 
-fetch('http://localhost:8010/api/foo')
-.then((res) => res.json())
-.then(data => {
-    data.forEach(item => {
-       // console.log(item.content)
-    })
-})
+// })
 
-fetch('http://localhost:8010/api/july17')
-.then((res) => res.json())
-.then(data => {
-    data.forEach(item => {
-        //console.log(item)
-        console.log(item.date)
-    })
-})
+// fetch('http://localhost:8010/api/july17')
+// .then((res) => res.json())
+// .then(data => {
+//     data.forEach(item => {
+//         //console.log(item)
+//         console.log(item.date)
+//     })
+// })
+
+document.querySelector('.wordRepeat').addEventListener('click', runRequest)
+
+//TODO add data from outlander api to mongoDB so this works again :(
+async function runRequest(){
+    try {
+        const response = await fetch('/api/foo')
+        const data = await response.json()
+        console.log(data)
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+//randomWord = getContent[Math.floor(Math.random() * getContent.length)]
+
 
 //confirmed tts does work now need to make it with the input from foo instead
 
+// let randomWord = wordData[Math.floor(Math.random() * wordData.length)]
 
-const textInputField = document.querySelector("#text-input")
+// console.log(randomWord)
+
+
+const textField = document.querySelector(".word")
 const form = document.querySelector("#form")
 const utterThis = new SpeechSynthesisUtterance()
 const synth = window.speechSynthesis
@@ -36,12 +65,12 @@ const checkBrowserCompatibility = () => {
 checkBrowserCompatibility()
 
 
-document.getElementById('form').addEventListener('click', run)
+// document.querySelector('.wordRepeat').addEventListener('click', run)
 
-function run (){
-    ourText = textInputField.value
-      utterThis.text = ourText
-        synth.speak(utterThis)
-        textInputField.value = ""
-}
+// function run (){
+//     ourText = textField.value
+//       utterThis.text = ourText
+//         synth.speak(utterThis)
+//         textField.value = ""
+// }
 
